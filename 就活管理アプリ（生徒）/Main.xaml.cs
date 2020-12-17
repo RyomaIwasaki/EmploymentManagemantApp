50feb0d28bfa6d9a8d49a0d535a74725b31350c8
RyomaIwasaki <31120@ojs.ac.jp>
2020-12-17 11:39:44 +0900

    プロジェクト ファイルを追加します。

---
+++ 就活管理アプリ（生徒）/Main.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace 就活管理アプリ_生徒_ {
+    /// <summary>
+    /// Main.xaml の相互作用ロジック
+    /// </summary>
+    public partial class Main : Window {
+        public Main() {
+            InitializeComponent();
+        }
+
+        private void Informationsession_Copy1_Checked(object sender, RoutedEventArgs e) {
+
+        }
+    }
+}
